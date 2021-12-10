75612b6061444a5d355e475b6d4f6a5351bdf25d
StefanoMarchesi <95907613+StefanoMarchesin@users.noreply.github.com>
2021-12-10 10:40:59 +0000

    esercizio

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import sys
+import math
+import cmath
+
+print("pi =", math.pi)
+
+print("e =", math.e)
+
+print(math.sin(math.pi / 2))
+
+r = 10 + 5j
+print(r)
+
+a = True
+type(a)
+print(a)
+
+b = False
+type(b)
+print(b)
+
+print(a and b)
+
+print(a or b)
