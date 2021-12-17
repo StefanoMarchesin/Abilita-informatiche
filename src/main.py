5884cb3bd74afb6fa7a9e2f69e6af692216864db
StefanoMarchesi <95907613+StefanoMarchesin@users.noreply.github.com>
2021-12-17 10:12:15 +0000

    nuovo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 import math
 import cmath
+import time
 
 print("pi =", math.pi)
 
@@ -11,7 +12,8 @@ print(math.sin(math.pi / 2))
 r = 10 + 5j
 print(r)
 
-#
+print()
+#boolean
 
 a = True
 type(a)
@@ -25,7 +27,8 @@ print(a and b)
 
 print(a or b)
 
-#
+print()
+#liste
 
 lista = [
     {"nome": "uno", "eta": 1},
@@ -34,3 +37,38 @@ lista = [
         ]
 for i in range(len(lista)):
     print("Nome %s, età %s" % (lista[i]["nome"], lista[i]["eta"]))
+
+T1 = time.time()
+
+l1 = [1, 2]
+l2 = ['a', 'b']
+l3 = [3]
+l = [(e1, e2, e3) for e1 in l1 for e2 in l2 for e3 in l3]
+print(l)
+
+l4 = range(10)
+list(l4)
+print(l4)
+l = [el*2 for el in l4]
+print(l)
+l.append(20)
+print(l)
+
+T2 = time.time()
+print('tempo di esequzione', T2-T1, 's')
+
+l.pop()
+print(l)
+
+print()
+#tuple
+
+tub = (1,2,3)
+#tub.append(4)
+print(tub)
+
+print()
+#dizionari
+
+d = {'nome': 'Mario', 'cognome': 'Rossi'}
+print(d['nome'])
